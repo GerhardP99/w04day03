777dd8fc706af92ae90aee7551e69f5a4ce466a9
GerhardP99 <gerhard-99@gmx.at>
2020-07-22 15:51:38 +0200

    dayend

--- src/gerhard/A5/Product.java
+++ src/gerhard/A5/Product.java
@@ -3,50 +3,36 @@ package gerhard.A5;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Product {
     String name;
     double price;
     int quantity;
     String description;
-    static String filePath = "C:\\Users\\gerha_nzlw7h1\\Java-Developer\\week04\\day03\\src\\gerhard\\my_files\\products.txt";
+    static String filePath = "C:\\Users\\gerha_nzlw7h1\\Java-Developer\\week04\\w04day03\\src\\gerhard\\my_files\\products.txt";
 
     @Override
     public String toString() {
-        return "Product{" +
-                "name='" + name + '\'' +
-                ", price=" + price +
-                ", quantity=" + quantity +
-                ", description='" + description + '\'' +
-                '}';
+        return "Product" +
+                "name = " + name +
+                " | price = " + price +
+                " | quantity = " + quantity +
+                " | description = " + description;
     }
 
     public Product(String n, double p, int q, String d){
-        /*n = name;
-        p = price;
-        q = quantity;
-        d = description;*/
         this.name = n;
         this.price = p;
         this.quantity = q;
         this.description = d;
     }
 
-    public static void printReport(){
+    public static void printReport(ArrayList<Product> products){
         try {
+            /*for(Product str:products){*/
             FileWriter file = new FileWriter(filePath);
-            file.write(String.valueOf(product1));
-            file.write(System.lineSeparator());
-            file.write(String.valueOf(product2));
-            file.write(System.lineSeparator());
-            file.write(String.valueOf(product3));
-            file.write(System.lineSeparator());
-            file.write(String.valueOf(product4));
-            file.write(System.lineSeparator());
-            file.write(String.valueOf(product5));
-            file.write(System.lineSeparator());
-            file.close();
+            file.write(products.toString());
+            file.close();/*}*/
 
             System.out.println("Successfully wrote to file.");
         } catch (IOException e) {
@@ -56,11 +42,11 @@ public class Product {
     }
 
     public static void main(String[] args) {
-        Product product1 = new Product("evian",1.25,10,"water");
-        Product product2 = new Product("milka",2.50,25,"chocolate");
-        Product product3 = new Product("allinson's",5.35,12,"bred");
-        Product product4 = new Product("Coca-Cola",2.00,50,"soft drink");
-        Product product5 = new Product("chiappetti",12.22,125,"deluxe meat");
+        Product product1 = new Product("evian",1.25,10,"water\n");
+        Product product2 = new Product("milka",2.50,25,"chocolate\n");
+        Product product3 = new Product("allinson's",5.35,12,"bred\n");
+        Product product4 = new Product("Coca-Cola",2.00,50,"soft drink\n");
+        Product product5 = new Product("chiappetti",12.22,125,"deluxe meat\n");
 
         ArrayList<Product> products = new ArrayList<Product>();
         products.add(product1);
@@ -69,6 +55,7 @@ public class Product {
         products.add(product4);
         products.add(product5);
 
+        printReport(products);
 
         /*for (int i = 0; i < products.size(); i++) {
             System.out.println(products.get(i));
